cfcee5f84188f6e6511692c662ef44c18909e554
Nick marriotti <nmarriotti@gmail.com>
2021-02-15 14:45:48 -0500

    added backup/restore feature

--- patch.ps1
+++ patch.ps1
@@ -9,6 +9,38 @@ param (
 $PATCHED_FILES_DIR = "$PSScriptRoot\files"
 $MANIFEST_CONFIG = "$PSScriptRoot\manifest.cfg"
 $MANIFEST = "$PSScriptRoot\manifest"
+$BACKUP_DIR = "$PSScriptRoot\backup"
+
+Function Menu()
+{
+    cls
+    Write-Host "Patch Utility Main Menu"
+    Write-Host "      1. Patch"
+    Write-Host "      2. Restore"
+    Write-Host "      3. Exit"
+    $action = Read-Host -Prompt "Select one"
+    return $action
+}
+
+
+Function BuildPath()
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [String]$Path,
+        [Parameter(Mandatory=$true)]
+        [String]$Base
+    )
+
+    $parts = $Path -split "\\"
+    $output = $parts[1..$parts.Count] -join '\'
+
+    # This is where the patch will be stored
+    $patch_path = "$Base\$output"
+
+    return $patch_path
+}
+
 
 Function WriteManifestLine()
 {
@@ -17,28 +49,14 @@ Function WriteManifestLine()
         [String]$Dest
     )
 
-    Function BuildPath()
-    {
-        param (
-            [Parameter(Mandatory=$true)]
-            [String]$Path
-        )
 
-        $parts = $Path -split "\\"
-        $output = $parts[1..$parts.Count] -join '\'
 
-        # This is where the patch will be stored
-        $patch_path = "$PATCHED_FILES_DIR\$output"
-
-        return $patch_path
-    }
-
-    echo "adding $Dest to manifest"
+    Write-Host "adding $Dest to manifest"
 
     # calculate md5 hash
     $md5 = Get-FileHash -Algorithm MD5 -Path $Dest
     # get absolute path where data will be stored in patch
-    $src = BuildPath -Path $Dest
+    $src = BuildPath -Path $Dest -Base $PATCHED_FILES_DIR
     
     # copy files/folders to patch directory
     New-Item -Path $(Split-Path -Path $src) -ItemType directory -ErrorAction SilentlyContinue | Out-Null
@@ -65,6 +83,9 @@ Function WriteManifestLine()
 
 Function Build()
 {
+    # remove backup directory
+    Remove-Item -Path $BACKUP_DIR -Force -Confirm:$false -ErrorAction SilentlyContinue | Out-Null
+
     # Count number of files included in this patch
     $num_files = 0
 
@@ -83,7 +104,7 @@ Function Build()
         }
     }
 
-    echo "Building manifest..."
+    Write-Host "Building manifest..."
 
 
 
@@ -103,7 +124,7 @@ Function Build()
         # Check if this line is a folder
         if(Test-Path -Path $dest -PathType Container)
         {
-            echo "scanning $dest"
+            Write-Host "scanning $dest"
 
             # Find subfolders
             $subfolders = Get-ChildItem -Path $dest -Recurse -Directory -Force
@@ -124,15 +145,18 @@ Function Build()
             $num_files += 1
         }
     }
-    echo "added $num_files file(s)."
-    echo "`nbuild complete."
+    Write-Host "added $num_files file(s)."
+    Write-Host "`nbuild complete."
 }
 
 
 Function ApplyPatches()
 {
+    # Create backup directory
+    New-Item -Path $BACKUP_DIR -ItemType directory -ErrorAction SilentlyContinue | Out-Null
+
     $num_patched = 0
-    echo "Checking files..."
+    Write-Host "Checking files..."
 
     # Read each line in manifest
     $_manifest = Import-CSV -Path $MANIFEST -ErrorAction Stop
@@ -166,7 +190,8 @@ Function ApplyPatches()
                 if(-Not ($item.Hash -match $currentHash.Hash))
                 {
                     # Hash mismatch, replace the file
-                    echo "patching file $($item.Destination)"
+                    Backup -Dest $item.Destination     # first backup the file!
+                    Write-Host "patching file $($item.Destination)"
                     Copy-Item -Path $item.Source -Destination $item.Destination -Force
                     $num_patched += 1
                 }
@@ -174,7 +199,7 @@ Function ApplyPatches()
             else
             {
                 # File is missing, replace it
-                echo "adding $($item.Destination)"
+                Write-Host "adding $($item.Destination)"
 
                 # Check if the destination directory already exists
                 $path_to_create = Split-Path -Path $item.Destination
@@ -190,11 +215,68 @@ Function ApplyPatches()
     }
     if($num_patched -eq 0)
     {
-        echo "no action taken. all files are intact."
+        Write-Host "no action taken. all files are intact."
     }
     else
     {
-        echo "$num_patched file(s) patched."
+        Write-Host "$num_patched file(s) patched."
+    }
+}
+
+
+Function Backup()
+{
+        param (
+            [Parameter(Mandatory=$true)]
+            [String]$Dest
+        )
+        Write-Host "creating backup of $Dest"
+        # This is where the back will be saved
+        $backup_path = BuildPath -Path $Dest -Base $BACKUP_DIR
+        # create the directory structure
+        New-Item -Path $(Split-Path -Path $backup_path) -ItemType directory -ErrorAction SilentlyContinue | Out-Null
+        # backup the file
+        Copy-Item -Path $Dest -Destination $backup_path -Force | Out-Null
+}
+
+
+Function Restore()
+{
+    $num_restored = 0
+    Write-Host "restoring..."
+
+    # Read each line in manifest
+    $_manifest = Import-CSV -Path $MANIFEST -ErrorAction Stop
+
+    # Iterate over each item
+    foreach($item in $_manifest)
+    {
+        $restoreFile = BuildPath -Path $item.Destination -Base $BACKUP_DIR
+
+        # Check if restore file exists
+        if(Test-Path -Path $restoreFile -PathType Leaf)
+        {
+            if(Test-Path -Path $item.Destination -PathType Leaf)
+            {
+                # Compare hashes
+                $backup_md5 = $(Get-FileHash -Algorithm MD5 -Path $restoreFile).Hash
+                if(-Not ($backup_md5 -match $(Get-FileHash -Algorithm MD5 -Path $item.Destination).Hash))
+                {
+                    Write-Host "restoring $($item.Destination)"
+                    Copy-Item -Path $restoreFile -Destination $item.Destination -Force
+                    $num_restored++
+                }
+            }
+        }
+    }
+
+    if($num_restored -gt 0)
+    {
+        Write-Host "complete`n$num_restored file(s) restored."
+    }
+    else
+    {
+        Write-Host "no action taken. restore was not required"
     }
 }
 
@@ -211,16 +293,66 @@ if($Build)
 }
 else
 {
-    Add-Type -AssemblyName PresentationFramework
-    $resp = [System.Windows.MessageBox]::Show('Proceed with patching? This action cannot be undone.','Patch Utility','YesNoCancel','Question')
-    
-    switch ($resp) 
+    while($true)
     {
-        'Yes' 
+        $action = Menu
+        switch($action)
         {
-            ApplyPatches
-            read-host “`nPress ENTER to exit or close this window...”
-            break
+            '1' 
+            {
+                $c = Read-Host -Prompt "Proceed with patching? (y/n)"
+                switch($c)
+                {
+                    'y'
+                    {
+                        ApplyPatches
+                        read-host “Press ENTER to continue...”
+                        break
+                    }
+                    'n'
+                    {
+                        break
+                    }
+                    default
+                    {
+                        Write-Host "Invalid option!"
+                        sleep 2
+                    }
+                }
+                break
+            }
+            '2' 
+            {
+                $c = Read-Host -Prompt "Do you wish to undo all changes that were applied by the patch? (y/n)"
+                switch($c)
+                {
+                    'y'
+                    {
+                        Restore
+                        read-host “Press ENTER to continue...”
+                        break
+                    }
+                    'n'
+                    {
+                        break
+                    }
+                    default
+                    {
+                        Write-Host "Invalid option!"
+                        sleep 2
+                    }
+                }
+                break
+            }
+            '3'
+            {
+                exit 0
+            }
+            default
+            {
+                Write-Host "Invalid option!"
+            }
         }
     }
+
 }
